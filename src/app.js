33166248c0d642ddb3b40ebb27cf37b1d78612a3
Bae Ha-eun <dncl46@gmail.com>
2023-08-21 13:31:52 +0900

    로더완료

--- src/app.js
+++ src/app.js
@@ -4,3 +4,10 @@ import nyancat from './nyancat.jpg'
 document.addEventListener('DOMContentLoaded', () => {
   document.body.innerHTML = `<img src="${nyancat}" alt="nyancat">`
 })
+
+console.log(process.env.NODE_ENV)
+console.log(TWO) //2
+console.log(VERSION) // 'v.1.2.3'
+console.log(PRODUCTION) // true
+console.log(MAX_COUNT) // 999
+console.log(api.domain) // 'http://dev.api.domain.com'
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
 const path = require("path")
 const webpack = require('webpack')
+const childProcess = require("child_process")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 module.exports = {
   mode: "development",
@@ -15,7 +19,9 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          'style-loader',
+          process.env.NODE_ENV === 'production'
+            ? MiniCssExtractPlugin.loader
+            :'style-loader',
           'css-loader',
         ]
       },
@@ -32,7 +38,7 @@ module.exports = {
         use: {
           loader: 'url-loader', // url 로더를 설정한다
           options: {
-            publicPath: './dist/', // file-loader와 동일
+            // publicPath: './dist/', // file-loader와 동일
             name: '[name].[ext]?[hash]', // file-loader와 동일
             limit: 25000 // 5kb 미만 파일만 data url로 처리
           }
@@ -42,7 +48,35 @@ module.exports = {
   },
   plugins: [
     new webpack.BannerPlugin({
-      banner: () => `빌드날짜: ${new Date().toLocaleString()}`
-    })
-  ]
+      banner: () => `
+        빌드날짜: ${new Date().toLocaleString()}
+        Commit Version: ${childProcess.execSync('git rev-parse --short HEAD')}
+        Author: ${childProcess.execSync('git config user.name')}
+      `
+    }),
+    new webpack.DefinePlugin({
+      TWO: "1+1",
+      VERSION: JSON.stringify("v.1.2.3"),
+      PRODUCTION: JSON.stringify(false),
+      MAX_COUNT: JSON.stringify(999),
+      "api.domain": JSON.stringify("http://dev.api.domain.com"),
+    }),
+    new HtmlWebpackPlugin({
+      template: './index.html', // 템플릿 경로를 지정
+      templateParameters: {
+        env: process.env.NODE_ENV === 'development' ? '(dev)' : '',
+      },
+      minify: process.env.NODE_ENV === 'production' 
+      ? {
+        collapseWhitespace: true,
+        removeComments: true
+      } 
+      : false,
+    }),
+    new CleanWebpackPlugin(),
+    ...(process.env.NODE_ENV === 'production'
+      ? [new MiniCssExtractPlugin({ filename: `[name].css` })]
+      : [])
+  ],
+  
 }
